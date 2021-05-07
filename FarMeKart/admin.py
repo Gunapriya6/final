922999b0ed7ab6294e648cd733c361706824e153
Gunapriya6 <guna11priya@gmail.com>
2021-05-07 09:56:19 +0530

    hello

---
+++ FarMeKart/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from FarMeKart.models import ExtPro,Vegpro,UserPro,Cart,Myorders
+# Register your models here.
+
+admin.site.register(ExtPro)
+admin.site.register(Vegpro)
+admin.site.register(UserPro)
+admin.site.register(Cart)
+admin.site.register(Myorders)
